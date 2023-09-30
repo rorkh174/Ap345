238285420249a790723e6d6061b872f79aeb6e78
ayhamkhateb <ayham.khateb@mail.huji.ac.il>
2023-09-30 09:32:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/app_ror/BackActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_ror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back);
+    }
+}
---
+++ app/src/main/java/com/example/app_ror/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.app_ror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToBack(View view) {
+        intent i = new intent(this, BackActivity.class)
+    }
+}
